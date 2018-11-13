4de2cecbffdb14c92f987ed197bcaf3194417272
Lanitta <GIT SERVICE EMAIL>
2018-11-12 20:31:55 -0700

    adding ball and animation

--- code.js
+++ code.js
@@ -9,6 +9,8 @@ let animate = window.requestAnimationFrame || function(callback){
 	const ctx = canvas.getContext("2d")
 	const player = new Player();
 	let keysDown = {};
+	const computer = new Computer();
+	const ball = new Ball(200, 300);
 
 
 	window.onload = function() {
@@ -24,13 +26,16 @@ let animate = window.requestAnimationFrame || function(callback){
 
 	let update = function() {
     player.update();
+		ball.update(player.paddle, computer.paddle);
 	};
 
 	let render = function() {
 		ctx.fillStyle = "#000000";
 		ctx.fillRect(0,0, width, height);
 		player.render();
-	}
+		ball.render();
+		computer.render();
+	};
 
 
 	//center line
@@ -59,24 +64,39 @@ let animate = window.requestAnimationFrame || function(callback){
 
 	function Player() {
 		this.paddle = new Paddle(175, 580, 50, 10);
-	}
-/*
+	};
+
 	function Computer() {
 		this.paddle = new Paddle(750, 750, 225, 275);
-	}
-*/
+	};
+
 
 Player.prototype.render = function() {
 	this.paddle.render()
-}
+};
 
-/*
-Computer.protypte.render = function(){
+
+Computer.prototype.render = function(){
   this.paddle.render();
-}
-*/
+};
 
-//Paddle Controls
+//ball
+function Ball(x, y){
+	this.x = x;
+	this.y = y;
+	this.x_speed = 0;
+	this.y_speed = 3;
+	this.radius = 5;
+};
+
+Ball.prototype.render = function(){
+	ctx.beginPath();
+	ctx.arc(this.x, this.y, this.radius, 0 * Math.PI, 2 * Math.PI)
+	ctx.fillStyle = 'white';
+	ctx.fill();
+};
+
+//Paddle Controls and Ball Controls
 window.addEventListener("keydown", function(even){
 	keysDown[event.keyCode] = true;
 });
@@ -113,3 +133,48 @@ Paddle.prototype.move = function(x, y) {
     this.x_speed = 0;
   }
 }
+
+Ball.prototype.update = function() {
+	this.x += this.x_speed;
+	this.y += this.y_speed;
+};
+
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
+  var top_x = this.x - 5;
+  var top_y = this.y - 5;
+  var bottom_x = this.x + 5;
+  var bottom_y = this.y + 5;
+
+  if(this.x - 5 < 0) { // hitting the left wall
+    this.x = 5;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 5 > 400) { // hitting the right wall
+    this.x = 395;
+    this.x_speed = -this.x_speed;
+  }
+
+  if(this.y < 0 || this.y > 600) { // a point was scored
+    this.x_speed = 0;
+    this.y_speed = 3;
+    this.x = 200;
+    this.y = 300;
+  }
+
+  if(top_y > 300) {
+    if(top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
+      // hit the player's paddle
+      this.y_speed = -3;
+      this.x_speed += (paddle1.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  } else {
+    if(top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
+      // hit the computer's paddle
+      this.y_speed = 3;
+      this.x_speed += (paddle2.x_speed / 2);
+      this.y += this.y_speed;
+    }
+  }
+};
